d17b98406c75812163eeb15de4b200a2a4a48356
oscilococcinum <bargaw28@gmail.com>
2026-09-20 18:20:20 +0200

    add autohide flag to getSelectionEx; format

--- utils/utils.py
+++ utils/utils.py
@@ -1,20 +1,23 @@
 # SPDX-License-Identifier: LGPL-2.1-or-later
-#pyright: standard, reportUnusedImport=error, reportMissingImports=information
-import Part #type: ignore
+# pyright: standard, reportUnusedImport=error, reportMissingImports=information
+from collections import defaultdict
+from collections.abc import Callable
 from functools import wraps
 from time import time
-from collections.abc import Callable
 from typing import Any
-from utils.FreeCADInterfaces import FeatureLike, ShapeLike
-import FreeCADGui as Gui
+
 import FreeCAD as App
-from utils.FreeCADInterfaces import Vector, ShapeLike
-from collections import defaultdict
+import FreeCADGui as Gui
+import Part  # type: ignore
+
 from utils.FaceDef import FaceDef
+from utils.FreeCADInterfaces import FeatureLike, ShapeLike, Vector
 from utils.VertexDef import VertexDef
 
 
-def getReferencedShapes(refShapes: list[tuple[FeatureLike, tuple[str]]]) -> list[ShapeLike]:
+def getReferencedShapes(
+    refShapes: list[tuple[FeatureLike, tuple[str]]],
+) -> list[ShapeLike]:
     shapes: list[ShapeLike] = []
 
     for feature, subnames in refShapes:
@@ -24,35 +27,41 @@ def getReferencedShapes(refShapes: list[tuple[FeatureLike, tuple[str]]]) -> list
 
     return shapes
 
-def getSelectionEx() -> list[tuple[FeatureLike, tuple[str]]]:
+
+def getSelectionEx(hideSelection: bool = False) -> list[tuple[FeatureLike, tuple[str]]]:
     refs = []
     for sel in Gui.Selection.getSelectionEx():
         if sel.SubElementNames:
             refs.append((sel.Object, sel.SubElementNames))
         else:
-            refs.append((sel.Object, ('')))
+            refs.append((sel.Object, ("")))
+    if hideSelection:
+        for feature, _ in refs:
+            feature.Visibility = False
     return refs
 
-def timing(f: Callable[..., Any], args: bool=True) -> Callable[..., Any]:
+
+def timing(f: Callable[..., Any], args: bool = True) -> Callable[..., Any]:
     @wraps(f)
     def wrap(*args: list[Any], **kw: dict[Any, Any]):
-            ts = time()
-            result = f(*args, **kw)
-            te = time()
-            print('func:%r args:[%r, %r] took: %2.4f sec' % \
-                (f.__name__, args, kw, te-ts))
-            return result
+        ts = time()
+        result = f(*args, **kw)
+        te = time()
+        print("func:%r args:[%r, %r] took: %2.4f sec" % (f.__name__, args, kw, te - ts))
+        return result
+
     return wrap
 
-def timingWOArgs(f: Callable[..., Any], args: bool=True) -> Callable[..., Any]:
+
+def timingWOArgs(f: Callable[..., Any], args: bool = True) -> Callable[..., Any]:
     @wraps(f)
     def wrap(*args: list[Any], **kw: dict[Any, Any]):
-            ts = time()
-            result = f(*args, **kw)
-            te = time()
-            print('func:%r took: %2.4f sec' % \
-                (f.__name__, te-ts))
-            return result
+        ts = time()
+        result = f(*args, **kw)
+        te = time()
+        print("func:%r took: %2.4f sec" % (f.__name__, te - ts))
+        return result
+
     return wrap
 
 
@@ -74,7 +83,6 @@ def oldNormalOnShell(shell: ShapeLike, point: Vector) -> Vector:
             bestFace = face
             bestPoint = pointPairs[0][0]
 
-
     if bestFace is None or bestPoint is None:
         raise RuntimeError("Could not find closest face on shell")
 
@@ -83,14 +91,21 @@ def oldNormalOnShell(shell: ShapeLike, point: Vector) -> Vector:
     normal = bestFace.normalAt(u, v)
     normal.normalize()
 
-    return normal#, bestPoint, bestFace
+    return normal  # , bestPoint, bestFace
+
 
 def newNormalOnShell(shell: ShapeLike, point: Vector) -> Vector:
     if not shell.Faces:
         raise ValueError("Shape contains no faces")
 
-    hashToFace = {x.hashCode(): FaceDef(x.hashCode(), x, f"Face{i}") for i, x in enumerate(shell.Faces, start=1)}
-    hashToVert = {x.hashCode(): VertexDef(x.hashCode(), x, f"Vertex{i}") for i, x in enumerate(shell.Vertexes, start=1)}
+    hashToFace = {
+        x.hashCode(): FaceDef(x.hashCode(), x, f"Face{i}")
+        for i, x in enumerate(shell.Faces, start=1)
+    }
+    hashToVert = {
+        x.hashCode(): VertexDef(x.hashCode(), x, f"Vertex{i}")
+        for i, x in enumerate(shell.Vertexes, start=1)
+    }
 
     faceToVertex = {f: f.Vertexes for f in shell.Faces}
     verToFaces: dict[ShapeLike, list[ShapeLike]] = defaultdict(list)
@@ -101,23 +116,22 @@ def newNormalOnShell(shell: ShapeLike, point: Vector) -> Vector:
 
     surfaceVerts = [x for x in shell.Vertexes]
     surfaceDists = [(point - x.Point).Length for x in shell.Vertexes]
-    #print(surfaceDists)
+    # print(surfaceDists)
     index = surfaceDists.index(min(surfaceDists))
-    #print(index)
+    # print(index)
     closestSurfaces = verToFaces[surfaceVerts[index]]
-    #print(closestSurfaces)
+    # print(closestSurfaces)
 
     bestDistance = float("inf")
     bestFace = None
 
     for face in closestSurfaces:
         distance, _, _ = App.Vertex(point).distToShape(face)
-        #print(distance)
+        # print(distance)
 
         if distance < bestDistance:
             bestDistance = distance
             bestFace = face
-
 
     if bestFace is None:
         raise RuntimeError("Could not find closest face on shell")
