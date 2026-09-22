0e97323f11a66325b1c017f8a08561296d5de3fb
oscilococcinum <gawlikbar@gmail.com>
2026-09-22 16:14:49 +0200

    alow  adding new seeed to sel and rerunning

--- TangentSelection/Proxy.py
+++ TangentSelection/Proxy.py
@@ -15,13 +15,13 @@ def proxyCommand() -> None:
             currentShape: ShapeLike = sel[0][0].Shape
             facesToJoin: list[ShapeLike] = getReferencedShapes(sel)
             tgTrack = FaceWalker(currentShape)
-            tgHashFaces: list[int] = tgTrack.walkTangent(facesToJoin[0].hashCode(), 1e-2)
+            tgHashFaces: list[int] = tgTrack.walkTangent(facesToJoin[-1].hashCode(), 1e-2)
             Gui.Selection.addSelection(sel[0][0], [tgTrack.HashToFace[f].name for f in tgHashFaces])
         case "Edge":
             currentShape: ShapeLike = sel[0][0].Shape
             edgesToJoin: list[ShapeLike] = getReferencedShapes(sel)
             tgTrack = EdgeWalker(currentShape)
-            tgHashEdges: list[int] = tgTrack.walkTangent(edgesToJoin[0].hashCode(), 1e-2)
+            tgHashEdges: list[int] = tgTrack.walkTangent(edgesToJoin[-1].hashCode(), 1e-2)
             Gui.Selection.addSelection(sel[0][0], [tgTrack.HashToEdge[f].name for f in tgHashEdges])
         case _:
             raise RuntimeError("Select face or edge!")
